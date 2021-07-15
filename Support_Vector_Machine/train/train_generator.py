0674738a97a1bda5ca39115907f3fb7b8913173a
tommy-otsu <naoki_yamada@example.com>
2021-07-15 22:16:32 +0900

    add jupyter notebook code

---
+++ Support_Vector_Machine/train/train_generator.py
@@ -0,0 +1,15 @@
+python -m pip install scikit-learn
+
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+
+X =1
+print(X)
