ef79d553af930a67bf84a80e92f7e75de83ccacf
beppy-hiro <hirofumi.beppy@gmail.com>
2021-10-10 21:44:26 +0900

    add a model

--- CNN/train.py
+++ CNN/train.py
@@ -26,26 +26,26 @@ x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 
 ##モデルを定義する
-# model = Sequential()
+model = Sequential()
 
-# ##Conv②Dで2次元レイヤーを表現する
-# model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=(28,28,1)))
+##Conv②Dで2次元レイヤーを表現する
+model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=(28,28,1)))
 
 
-# # (2,2)のマックスプーリング層を追加
-# model.add(MaxPooling2D(pool_size=(2, 2)))
+# (2,2)のマックスプーリング層を追加
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-# # 20%をドロップするDropOut層を追加
-# model.add(Dropout(0.2))
+# 20%をドロップするDropOut層を追加
+model.add(Dropout(0.2))
 
-# # データを１次元にする
-# model.add(Flatten())
+# データを１次元にする
+model.add(Flatten())
 
-# # 出力：128次元の全結合層とReLU層を追加
-# model.add(Dense(128, activation='relu'))
+# 出力：128次元の全結合層とReLU層を追加
+model.add(Dense(128, activation='relu'))
 
-# # 30%をドロップするDropOut層を追加
-# model.add(Dropout(0.3))
+# 30%をドロップするDropOut層を追加
+model.add(Dropout(0.3))
 
-# #0~9の10次元の出力層とsoftmax層を追加
-# model.add(Dense(10, activation='softmax'))
+#0~9の10次元の出力層とsoftmax層を追加
+model.add(Dense(10, activation='softmax'))
